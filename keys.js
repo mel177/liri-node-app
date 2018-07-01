a0077b72159e3fab7ac39971a11330d8367af7ad
Melinh Mai <melinh.mai@gmail.com>
2018-06-30 23:15:47 -0500

    js files

---
+++ keys.js
@@ -0,0 +1,13 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+    consumer_key: 'Q6MEmLnNx3tfkLWVffEdY6tEd',
+    consumer_secret: 'd5axOJDlV4hjQngdUgm9CJpnsJKX1btT3EWOaDxWJTgjvj31GD',
+    access_token_key: '1013259749004103681-WXbfsdGyjYmj5GbOnKfcAFXZNAswj2',
+    access_token_secret: 'NUYdxqp6zb4ud3JGEvHxezx5qyZUlNkXMYcOeR9QO2pzc',
+};
+
+exports.spotify = {
+    id: 'c0839839cc5745b3acfbd890e55d516e',
+    secret: 'dd81a646dbe4441aa55f676dda9f92c5'
+  };
