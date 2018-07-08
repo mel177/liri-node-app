cc399a6a9da6c969582bf95fb944e3cefa34256c
Melinh Mai <melinh.mai@gmail.com>
2018-07-08 15:52:53 -0500

    stuff

--- keys.js
+++ keys.js
@@ -1,13 +1,16 @@
 console.log('this is loaded');
 
-exports.twitterKeys = {
-    consumer_key: 'Q6MEmLnNx3tfkLWVffEdY6tEd',
-    consumer_secret: 'd5axOJDlV4hjQngdUgm9CJpnsJKX1btT3EWOaDxWJTgjvj31GD',
-    access_token_key: '1013259749004103681-WXbfsdGyjYmj5GbOnKfcAFXZNAswj2',
-    access_token_secret: 'NUYdxqp6zb4ud3JGEvHxezx5qyZUlNkXMYcOeR9QO2pzc',
+  exports.twitterKeys = {
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key:process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
 exports.spotify = {
-    id: 'c0839839cc5745b3acfbd890e55d516e',
-    secret: 'dd81a646dbe4441aa55f676dda9f92c5'
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
   };
+
+
+
--- liri.js
+++ liri.js
@@ -1,11 +1,29 @@
 //using .env to hide 
-// require("dotenv").config();
+require("dotenv").config();
 
 var keys = require('./keys');
 var twitterCredentials = keys.twitterKeys;
+var Spotify = require('node-spotify-api');
+var request = require("request");
+
+var searchTerm = '';
+for(var i = 3; i < process.argv.length; i++){
+    searchTerm += " " + process.argv[i];
+}
+
+console.log("This is our Search Term: " + searchTerm)
 
 var command = process.argv[2];
-var query = process.argv[3];
+// var query = process.argv[3];
+
+var query = searchTerm;
+
+
+
+var bacon = new Spotify({
+    id: keys.spotify.id,
+    secret: keys.spotify.secret
+})
 
 // function for 3 main functions of the liri app
 
@@ -46,7 +64,38 @@ var myTweets = function() {
     });
 }
 
+var spotifyThisSong = function(trackQuery){
+
+if (trackQuery === undefined){
+    trackQuery = "the sign ace of base";
+
+}
+
+// Spotify API request (if an object is returned, output the first search result's artist(s), song, preview link, and album)
+bacon.search({ type: 'track', query: trackQuery }, function(error, data) {
+    if(error) { // if error
+        console.log('Error occurred: ' + error);
+    } else { // if no error
+        // For loop is for when a track has multiple artists
+
+        
+        console.log("Song: ", data.tracks.items[0].name);
+        console.log("Artist: ", data.tracks.items[0].album.artists[0].name)
+        console.log("Album: ", data.tracks.items[0].album.name)
+        console.log("Preview URL: ", data.tracks.items[0].album.artists[0].external_urls.spotify)
+
+            
+    }
+ 
+         
+});
+}
 // what the user will use to call the commands
 if(command === "my-tweets"){
     myTweets();
 }
+else if (command === "spotify-this-song"){
+    spotifyThisSong(query);
+}
+
+
