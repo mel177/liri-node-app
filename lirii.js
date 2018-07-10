cd646a0b8351a306929184d355c1b21309df01ae
Melinh Mai <melinh.mai@gmail.com>
2018-07-10 01:00:14 -0500

    rename

--- lirii.js
+++ lirii.js
@@ -93,6 +93,39 @@ bacon.search({ type: 'track', query: trackQuery }, function(error, data) {
          
 });
 }
+// movie function 
+function movieFinder(){
+    var movie = process.argv[3];
+    if(!movie){
+        movie = "frozen";
+    }
+    params = movie
+    request("http://www.omdbapi.com/?t=" + params + "&y=&plot=short&r=json&tomatoes=true", function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            var movieObject = JSON.parse(body);
+            //console.log(movieObject); // Show the text in the terminal
+            var movieResults =
+            "Title: " + movieObject.Title+"\r\n"+
+            "Year: " + movieObject.Year+"\r\n"+
+            "Imdb Rating: " + movieObject.imdbRating+"\r\n"+
+            "Country: " + movieObject.Country+"\r\n"+
+            "Language: " + movieObject.Language+"\r\n"+
+            "Plot: " + movieObject.Plot+"\r\n"+
+            "Actors: " + movieObject.Actors+"\r\n"+
+            "Rotten Tomatoes Rating: " + movieObject.tomatoRating+"\r\n"+
+            "Rotten Tomatoes URL: " + movieObject.tomatoURL + "\r\n" + 
+            
+            console.log(movieResults);
+            log(movieResults); // calling log function
+        } else {
+            console.log("Error :"+ error);
+            return;
+        }
+    });
+};
+
+
+
 
 //  do what it says function, reads and write module, access random.txt
 function doWhatItSays(){
